8d1903a9b52add5bdfc921d5bcf6305e11075c9f
Mauro Corominas <maurocorominas@gmail.com>
2023-08-21 21:16:32 -0300

    asignamos port para render

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path = require("path")
 
 app.use(express.static(path.join(__dirname,"../public")));
 
-
+const port = process.env.PORT || 3001;
 
 const PORT = 3001;
 
@@ -16,3 +16,5 @@ app.listen(PORT, ()=>{
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"views/home.html"));
 })
+
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`));
